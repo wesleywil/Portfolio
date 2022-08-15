94dd90ceda30cbeb1602fad8097112b87899b179
wesleywil <wesleywilsonti@gmail.com>
2022-08-15 18:04:26 -0300

    added project relation serializer to profile serializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,7 +1,9 @@
+
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from .models import Profile 
+from .models import Profile
+from projects.serializers import ProjectSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,6 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    projects = ProjectSerializer(read_only = True, many=True)
     class Meta:
         model = Profile
         fields =('id', 'user', 'picture', 'github_link', 'projects')
