cf2e5829273b8dbb6fa7cd74329cc08d0d31275c
wesleywil <wesleywilsonti@gmail.com>
2022-08-17 15:39:30 -0300

    updated homepage

--- frontend/src/pages/homepage/homepage.jsx
+++ frontend/src/pages/homepage/homepage.jsx
@@ -1,7 +1,20 @@
 const Homepage = () => {
   return (
     <div>
-      <h1 className="text-red-400 text-3xl text-center">Home Page!</h1>
+      <div className="pt-56 text-8xl text-center text-white">
+        <h1>Hi,</h1>
+        <h1>
+          I'm <span className="text-red-400">W</span>esley{" "}
+          <span className="text-red-400">W</span>ilson
+        </h1>
+        <h1>A web developer</h1>
+        <h2 className="text-3xl text-slate-200 ">Django + Reactjs</h2>
+        <div className="mt-4">
+          <button className=" text-2xl border-2 text-red-400 hover:text-red-500 border-red-400 hover:border-red-500 p-2 font-semibold rounded-xl">
+            Contact Me!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
