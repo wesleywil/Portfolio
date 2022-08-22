75dbf8348868e180479fd412b559bb0043ffcf2d
wesleywil <wesleywilsonti@gmail.com>
2022-08-22 17:53:35 -0300

    updated skill page and added a tooltip in the skillbar component

--- frontend/src/components/skill_bar/skill_bar.component.jsx
+++ frontend/src/components/skill_bar/skill_bar.component.jsx
@@ -1,7 +1,14 @@
-const SkillBar = ({ text, progress }) => {
+const SkillBar = ({ text, progress, text2 }) => {
   return (
     <div>
-      <h1 className="text-left text-white">{text}</h1>
+      <div className="flex">
+        <h1 className="text-left text-white text-xl">{text}</h1>
+        <span
+          className="tooltip  tooltip-right right-4 p-2"
+          data-tip={text2}
+        ></span>
+      </div>
+
       <div className="text-left">
         <progress
           className="progress progress-error "
--- frontend/src/pages/skills/skills.jsx
+++ frontend/src/pages/skills/skills.jsx
@@ -1,27 +1,30 @@
+import { FaReact, FaPython, FaRust, FaJava, FaPhp } from "react-icons/fa";
+import { SiDjango, SiJavascript } from "react-icons/si";
+
 import SkillBar from "../../components/skill_bar/skill_bar.component";
 import ContactTip from "../../components/contact_tip/contact_tip.component";
 
 const Skills = () => {
   return (
-    <div className="text-center pt-10 xl:pt-52 mx-auto flex flex-col xl:flex-row gap-2 justify-center">
+    <div className="text-center pt-10 xl:pt-52 mx-auto flex flex-col xl:flex-row gap-2 justify-center ">
       <h1 className="block xl:hidden md:hidden text-7xl mb-5 mt-1 text-white text-center">
         <span className="text-red-400 font-bold">S</span>kills
       </h1>
-      <div className="hidden xl:block md:block pl-10 w-1/2 md:w-full md:p-2">
+      <div className="hidden xl:block md:block pl-10 w-1/2 xl:w-1/2 md:w-full md:p-2">
         <h1 className="text-7xl mb-5 mt-1 text-white text-left md:text-center">
           <span className="text-red-400 font-bold">S</span>kills
         </h1>
-        <p className="mb-2 text-white text-xl text-left md:text-center">
+        <p className="mb-2 text-white text-xl xl:py-2  md:text-center xl:text-left">
           I create amazing web apps that are fast and easy to use. The main
           technologies used in my apps are Html, CSS, Javascript to Front-end
           and manly Python to Back-end.
         </p>
-        <p className="mb-2 text-white text-xl text-left md:text-center">
+        <p className="mb-2 text-white text-xl xl:py-2 xl:text-left md:text-center">
           I like to experience with other tech as well, that's why I'm using
           nowadays Tauri an amazing framework to create desktop apps using rust
           as the backend language and reactjs at the Front-end.
         </p>
-        <div className="text-left md:text-center">
+        <div className="xl:text-left md:text-center">
           <ContactTip />
         </div>
       </div>
@@ -29,18 +32,22 @@ const Skills = () => {
         <div className="border-2 border-slate-400 p-2 rounded-xl">
           <SkillBar text={"Front-end"} progress={60} />
           <SkillBar text={"Back-end"} progress={55} />
-          <SkillBar text={"Reactjs"} progress={50} />
-          <SkillBar text={"Django"} progress={60} />
+          <SkillBar text={<FaReact />} progress={50} text2={"Reactjs"} />
+          <SkillBar text={<SiDjango />} progress={60} text2={"Django"} />
         </div>
         <h1 className="text-left text-white font-bold text-xl my-2">
           Programming Languages
         </h1>
         <div className="border-2 border-slate-400 p-2 rounded-xl">
-          <SkillBar text={"Javascript"} progress={60} />
-          <SkillBar text={"Python"} progress={70} />
-          <SkillBar text={"Rust"} progress={20} />
-          <SkillBar text={"Java"} progress={40} />
-          <SkillBar text={"PHP"} progress={30} />
+          <SkillBar
+            text={<SiJavascript />}
+            progress={60}
+            text2={"Javascript"}
+          />
+          <SkillBar text={<FaPython />} progress={70} text2={"Python"} />
+          <SkillBar text={<FaRust />} progress={20} text2={"Rust"} />
+          <SkillBar text={<FaJava />} progress={40} text2={"Java"} />
+          <SkillBar text={<FaPhp />} progress={30} text2={"PHP"} />
           <SkillBar text={"Pascal"} progress={20} />
         </div>
         <h1 className="text-left text-white font-bold text-xl my-2">
